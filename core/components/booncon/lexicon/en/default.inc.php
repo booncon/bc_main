a89e54a761b4bd27199af3582c0064b5cda0c6d8
Sven Perkmann <sven@booncon.com>
2013-04-17 23:30:06 +0300

    mvp done

--- core/components/booncon/lexicon/en/default.inc.php
+++ core/components/booncon/lexicon/en/default.inc.php
@@ -1,8 +1,1 @@
 <?php
-// Possible categories of portfolio projects as defined in the template variable "category" (project namespace)
-$_lang['project.category.screen'] = 'screen';
-$_lang['project.category.ux'] = 'ux';
-$_lang['project.category.ui'] = 'ui';
-$_lang['project.category.brand'] = 'branding';
-$_lang['project.category.print'] = 'print';
-$_lang['title.client'] = 'Client:';
